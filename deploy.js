93293f72225150d502b020c2e00a9f53c2785a60
kongw <kongwela75@gmail.com>
2019-06-13 19:23:13 -0700

    updated .travis.yml and deploy.sh files

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '18.212.37.102',
+      host: '184.72.122.39',
       username: 'ubuntu',
       privateKey: 'kg-key.pem'
     })
